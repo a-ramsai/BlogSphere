22d872b3066307a30803b35f59aa5d09f32bed38
a-ramsai <a.ramsai24j@gmail.com>
2025-03-22 11:53:30 +0530

    final commit

--- client/src/components/common/Articles.jsx
+++ client/src/components/common/Articles.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useAuth } from '@clerk/clerk-react';
 import { useNavigate } from "react-router-dom";
-import { UserAuthorContextObj } from "../../context/";
+import { UserAuthorContextObj } from "../../context/UserAuthorContext";
 import { useContext } from 'react';
 
 function Articles() {
